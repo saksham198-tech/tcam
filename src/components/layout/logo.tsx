e7f64925d095fb130b271c32cf47e7a88dcf840d
Charak Srivastava <srivastavasaksham198@gmail.com>
2026-02-19 09:20:01 +0000

    change the background of logo image from black to transparent

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative overflow-hidden h-10 w-10", className)}>
+    <div className={cn("relative h-10 w-10 flex items-center justify-center", className)}>
       <Image
         src="https://res.cloudinary.com/dlndf5tb6/image/upload/v1771492469/WhatsApp_Image_2026-02-19_at_2.31.09_PM_uh2nqd.jpg"
         alt="TCAM Logo"
         width={40}
         height={40}
-        className="object-contain"
+        className="object-contain mix-blend-screen"
         priority
       />
     </div>
